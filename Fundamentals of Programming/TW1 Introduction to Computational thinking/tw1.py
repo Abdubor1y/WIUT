7267b0b0185ce4d47572d25767eda2eb4608eff2
Abduboriy <abduboriybahodirovv@gmail.com>
2025-11-03 21:57:33 +0500

    New module

---
+++ Fundamentals of Programming/TW1 Introduction to Computational thinking/tw1.py
@@ -0,0 +1,36 @@
+'''
+Exercise 1
+'''
+# Add two numbers
+
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
+# total = f"{number1} + {number2}"
+# print(f"Result: {total}")
+
+# What is different?
+
+# number1 = input("Enter first number: ")
+# number2 = input("Enter second number: ")
+# total = number1 + number2
+# print(f"Result: {total}")
+
+# Correct version
+
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
+# total = number1 + number2
+# print(f"Result: {total}")
+
+'''
+Exercise 2
+'''
+
+def sum_numbers(a, b):
+    return a + b
+print(sum_numbers(5, 10))
+
+# user_name = input("Enter your name: ")
+# print(f"Hi {user_name} let's start exercises!")
+
+
